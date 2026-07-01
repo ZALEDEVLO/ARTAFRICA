ae0e43c9f51d5fcf125ea2311af2d6ebd66df0c2
MAMADOU SALIOU SOW <zalesow27@gmail.com>
2026-07-01 16:19:01 +0000

    vrv

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def home(request):
-    return HttpResponse("Bienvenue sur mon projet Django bgh uf efef frgrgef !")
+    return HttpResponse("Bienvenue sur mon  !")
