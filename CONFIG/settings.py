c96547b6fb574fdb26674f1e9d1d3214eed4788a
zale24 <zalesow27@gmail.com>
2026-07-04 01:01:29 +0000

    ff

--- CONFIG/settings.py
+++ CONFIG/settings.py
@@ -114,7 +114,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "app/static",
-]
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR /'app/static']
